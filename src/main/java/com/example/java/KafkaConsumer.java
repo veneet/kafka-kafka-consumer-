6f3ae6c697ba534ab813989598bea48b3621f8d7
Veneet Pandey <veneetpandey@outlook.com>
2022-04-25 22:44:19 +0530

    Kafka consumer

---
+++ src/main/java/com/example/java/KafkaConsumer.java
@@ -0,0 +1,13 @@
+package com.example.java;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "NewTopic", groupId = "group_id")
+    public void consume(String message) {
+        System.out.println("message = " + message);
+    }
+}
